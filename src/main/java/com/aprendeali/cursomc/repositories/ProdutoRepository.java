fdc7cb2259522a385a03cd1942770cc6fb582d8d
AMULUNGO <mulungo.ali07@gmail.com>
2021-02-02 21:04:44 +0200

    Produtos  e Associacao muitos para muitos

---
+++ src/main/java/com/aprendeali/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.aprendeali.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aprendeali.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+}
