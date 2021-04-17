27512448a0667a7ae7d8c4f572c1e73615aa3d08
AMULUNGO <mulungo.ali07@gmail.com>
2021-04-17 16:05:40 +0200

    Customizacao da Validacao: email nao repetido

--- src/main/java/com/aprendeali/cursomc/domain/Cliente.java
+++ src/main/java/com/aprendeali/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -27,11 +28,12 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
+	@Column(unique = true)
 	private String email;
 	private String nuit;
 	private Integer tipoCliente;
 
-	
 	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
--- src/main/java/com/aprendeali/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/aprendeali/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,28 +6,42 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aprendeali.cursomc.domain.Cliente;
 import com.aprendeali.cursomc.domain.enums.TipoCliente;
 import com.aprendeali.cursomc.dto.ClienteNewDTO;
+import com.aprendeali.cursomc.repositories.ClienteRepository;
 import com.aprendeali.cursomc.resources.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
-		
+
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !MZ.isValidCPF(objDto.getNuit())) {
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !MZ.isValidCPF(objDto.getNuit())) {
 			list.add(new FieldMessage("nuit", "NUIT invalido	"));
 		}
-		
-		if(objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !MZ.isValidCNPJ(objDto.getNuit())) {
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !MZ.isValidCNPJ(objDto.getNuit())) {
 			list.add(new FieldMessage("nuit", "NUIT invalido	"));
 		}
-		
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email ja esxistente"));
+
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
